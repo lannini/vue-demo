aabf584b2833433f38e70b7cbc480d63add52c30
liaojulan <609737584@qq.com>
2020-03-13 19:50:26 +0800

    css

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="header">
+            <div class="header-left">111</div>
+            <div class="header-input">222</div>
+            <div class="header-right">333</div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        name:'HomeHeader'
+    }
+</script>
+
+<style lang="stylus" scoped>
+.header
+    display:flex
+    line-height:.86rem
+    background:#00bcd4
+    color:#fff
+    .header-left
+        width:.64rem
+        float:left
+        .header-right
+            width:1.24rem
+            float:right
+</style>
