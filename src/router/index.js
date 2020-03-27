eb086d2d4d2ef757a74b13beb206a32371a7e8be
liaojulan <609737584@qq.com>
2020-03-27 19:12:29 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-<<<<<<< HEAD
-=======
 import City from '@/pages/city/City'
->>>>>>> origin/zujian
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-<<<<<<< HEAD
-      name: 'HelloWorld',
-      component: Home
-    },{
-      path: '/city',
-      name: 'City',
-      component: City
-    }
-=======
-      name: 'Home',
-      component: Home
-    },{
-          path:'/city',
-          name:'City',
-          component:City
-      }
->>>>>>> origin/zujian
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  },
   ]
 })
