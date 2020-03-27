4f1aa6f070b1738703cb944402950036c211c759
liaojulan <609737584@qq.com>
2020-03-27 18:56:24 +0800

    city

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="header">
         城市选择
+        <router-link to="/">
+        <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
     </div>
 </template>
 
@@ -10,14 +13,24 @@
     }
 </script>
 
+
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl'
     .header
         overflow:hidden
-        height:.86rem
-        line-height:.86rem
+        height:$headerHeight
+        line-height:$headerHeight
         text-align:center
-        color:$bgColor
-        font-size:.4rem
+        color:#fff
+        background:$bgColor
+        font-size:.32rem
+    .header-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
 
 </style>
